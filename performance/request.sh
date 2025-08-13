95d7107a5ed450c6ade455557aa36d9752e23de8
fabenan-f <63860771+fabenan-f@users.noreply.github.com>
2025-08-13 09:42:14 +0200

    feat: automatically create, move and unzip results

--- performance/request.sh
+++
@@ -1,6 +0,0 @@
-curl -O -J --header "Content-Type: application/json" \
-  --connect-timeout 10240 \
-  --max-time 10240 \
-  --request POST \
-  --data '@request.json' \
-  http://localhost:8080/orbital/run
