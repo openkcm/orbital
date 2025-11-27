91c564dc8d26d27330bb312ef24438a1451024f7
jK <33685667+jithinkunjachan@users.noreply.github.com>
2025-11-27 17:03:02 +0100

    change solace image 10.25.9

--- client/amqp/amqp_test.go
+++ client/amqp/amqp_test.go
@@ -738,7 +738,7 @@ management.tcp.port = 15672
 
 func startSolace(ctx context.Context, opts ...containerOpts) (testcontainers.Container, error) {
 	req := testcontainers.ContainerRequest{
-		Image:        "solace/solace-pubsub-standard:latest",
+		Image:        "solace/solace-pubsub-standard:10.25.9",
 		ExposedPorts: []string{"5672/tcp"},
 		Env: map[string]string{
 			"username_admin_globalaccesslevel": "admin",
